a2f71d5e8bad6429f611349be2dfea12c80042a2
Matex600 <mat.leks@gmail.com>
2021-09-07 18:40:11 +0000

    add comments on overall game logic and generateQuestions functions in order to randomise questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,38 @@
 const playGame = document.getElementById("play-btn")
 const questionsBox = document.getElementById("questions")
 
+// get play button 
+// add eventlistener on click 
+// hide button update class
+// display quiz
+// get question from questions array randomly
+// display question 1
+// display possible answers 1
 
-function runQuiz(){
+function generateQuestions() {
+    const questions = [];
+    const correctAnswers = [];
+    const incorrectAnswers = [];
+    let questionsHoldingArray = questions
+         
+}
+
+function runQuiz() {
 
 }
 
-function inputQuestions(){
+function inputQuestions() {
 
 }
 
-function checkAnswer(){
+function checkAnswer() {
 
 }
 
-function updateScore(){
+function updateScore() {
 
 }
 
-function nextQuestion(){
+function nextQuestion() {
 
 }
-
-
-
