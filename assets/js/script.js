e942d58d1512e8bb2735366aa01a859cf495fbde
Matex600 <mat.leks@gmail.com>
2021-09-07 19:01:00 +0000

    improve array structure for better view

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,14 +11,25 @@ const questionsBox = document.getElementById("questions")
 
 function generateQuestions() {
     let gameQuestions = [];
-    const questions = ['What is the Empire city of Nuln known for?','Where do the halflings dwell?','Who leads the Dwarfs?','Who is the vampire lord of Sylvania','What other names are the Beastmen also known as?','Who are the Dawi-Zharr?','What is the home land of the druchii called?','What is the capital of the Skaven','Who is the primary worshipped deity in the Empire?','Who is the dark god of pestilence?'];
+    const questions = [
+        'What is the Empire city of Nuln known for?',
+        'Where do the halflings dwell?', 'Who leads the Dwarfs?',
+        'Who is the vampire lord of Sylvania',
+        'What other names are the Beastmen also known as?',
+        'Who are the Dawi-Zharr?',
+        'What is the home land of the druchii called?',
+        'What is the capital of the Skaven',
+        'Who is the primary worshipped deity in the Empire?',
+        'Who is the dark god of pestilence?'
+    ];
     let questionsHoldingArray = [...questions];
 
     x = 0;
     while (x > questions.length) {
         i = Math.floor(Math.random() * questionHoldingArray.length);
+
     }
-         
+
 }
 
 function runQuiz() {
