a7b8e714e0c9b3c61171afaefcf1649488ef5ba2
Matex600 <mat.leks@gmail.com>
2021-09-07 21:19:30 +0000

    replace single line comment for game logic with multi line

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,14 @@
 const playGame = document.getElementById("play-btn")
 const questionsBox = document.getElementById("questions")
 
-// get play button 
-// add eventlistener on click 
-// hide button update class
-// display quiz
-// get question from questions array randomly
-// display question 1
-// display possible answers 1
+/* get play button 
+ add eventlistener on click 
+ hide button update class
+ display quiz
+ get question from questions array randomly
+ display question 1
+ display possible answers 1
+*/ 
 
 function generateQuestions() {
     let gameQuestions = [];
