26128f6fb5e1bbb1ac2dad4a40246899bae784fc
Matex600 <mat.leks@gmail.com>
2021-09-08 21:10:25 +0000

    add score increaseElement

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ const questionsBox = document.getElementById("questions")
 const getQuestionElement = document.getElementById("question")
 const getAnswerElement = document.getElementById("answer")
 const getIncorrectAnswerElement = document.getElementById("incorrect")
+const scoreIncreaseElement = document.getElementById("add-point")
 
 
 playGame.addEventListener('click', runQuiz)
@@ -25,81 +26,81 @@ randomise questions and answers arrays to get
 different set of questions and answers every
 time you play */
 
-    let gameQuestions = [];
-    let gameAnswers = [];
-    let gameIncorrectAnswers = [];
-    const questions = [
-        'What is the Empire city of Nuln known for?',
-        'Where do the halflings call home?',
-        'Which of these dawi leads the Dwarfs?',
-        'Who is the vampire lord of Sylvania',
-        'What are the Beastmen also known as?',
-        'What is the English word forDawi-Zharr?',
-        'What is the home land of the druchii called?',
-        'What is the capital of the Skaven',
-        'What is the worshipped deity in the Empire?',
-        'Who is the dark god of pestilence?'
-    ];
-    let questionsHoldingArray = [...questions];
-    console.log(questionsHoldingArray.length);
-    const answers = [
-        'Imperial Gunnery school',
-        'The Moot',
-        'Thorgrim Grudgebearer',
-        'Manfredd von Carstein',
-        'The Cloven ones',
-        'Chaos Dwarfs',
-        'Naggaroth',
-        'Skavenblight',
-        'Sigmar',
-        'Nurgle'
-    ];
-    let answersHoldingArray = [...answers];
-    console.log(answersHoldingArray.length);
-    const incorrectAnswers = [
-        'Imperial Wizard school',
-        'Karak eight peaks',
-        'Ungrim Ironfist',
-        'Vlad von Carstein',
-        'The Old ones',
-        'Chaos Ogres',
-        'Ulthuan',
-        'Hellpit',
-        'Lady of the Lake',
-        'Khorne'
-    ];
-    i = 0
-    while (i < answers.length) {
-        answers[i] = true;
-        i++
-    }
-    
-    let incorrectAnswersHoldingArray = [...incorrectAnswers];
-    console.log(incorrectAnswersHoldingArray.length);
-    x = 0;
-    while (x < questions.length) {
-        i = Math.floor(Math.random() * questionsHoldingArray.length);
-        console.log(i);
-        temp = questionsHoldingArray[i];
-        tempAnswer = answersHoldingArray[i];
-        tempIncorrectAnswer = incorrectAnswersHoldingArray[i];
-        console.log(temp);
-        console.log(questionsHoldingArray);
-        console.log(tempAnswer);
-        console.log(answersHoldingArray);
-        console.log(tempIncorrectAnswer);
-        console.log(incorrectAnswersHoldingArray);
-        gameQuestions.push(temp);
-        gameAnswers.push(tempAnswer);
-        gameIncorrectAnswers.push(tempIncorrectAnswer);
-        questionsHoldingArray.splice(i, 1);
-        answersHoldingArray.splice(i, 1);
-        incorrectAnswersHoldingArray.splice(i, 1);
-        console.log(gameQuestions);
-        console.log(gameAnswers);
-        console.log(gameIncorrectAnswers);
-        x++;
-    }
+let gameQuestions = [];
+let gameAnswers = [];
+let gameIncorrectAnswers = [];
+const questions = [
+    'What is the Empire city of Nuln known for?',
+    'Where do the halflings call home?',
+    'Which of these dawi leads the Dwarfs?',
+    'Who is the vampire lord of Sylvania',
+    'What are the Beastmen also known as?',
+    'What is the English word forDawi-Zharr?',
+    'What is the home land of the druchii called?',
+    'What is the capital of the Skaven',
+    'What is the worshipped deity in the Empire?',
+    'Who is the dark god of pestilence?'
+];
+let questionsHoldingArray = [...questions];
+console.log(questionsHoldingArray.length);
+const answers = [
+    'Imperial Gunnery school',
+    'The Moot',
+    'Thorgrim Grudgebearer',
+    'Manfredd von Carstein',
+    'The Cloven ones',
+    'Chaos Dwarfs',
+    'Naggaroth',
+    'Skavenblight',
+    'Sigmar',
+    'Nurgle'
+];
+let answersHoldingArray = [...answers];
+console.log(answersHoldingArray.length);
+const incorrectAnswers = [
+    'Imperial Wizard school',
+    'Karak eight peaks',
+    'Ungrim Ironfist',
+    'Vlad von Carstein',
+    'The Old ones',
+    'Chaos Ogres',
+    'Ulthuan',
+    'Hellpit',
+    'Lady of the Lake',
+    'Khorne'
+];
+i = 0
+while (i < answers.length) {
+    answers[i] = true;
+    i++
+}
+
+let incorrectAnswersHoldingArray = [...incorrectAnswers];
+console.log(incorrectAnswersHoldingArray.length);
+x = 0;
+while (x < questions.length) {
+    i = Math.floor(Math.random() * questionsHoldingArray.length);
+    console.log(i);
+    temp = questionsHoldingArray[i];
+    tempAnswer = answersHoldingArray[i];
+    tempIncorrectAnswer = incorrectAnswersHoldingArray[i];
+    console.log(temp);
+    console.log(questionsHoldingArray);
+    console.log(tempAnswer);
+    console.log(answersHoldingArray);
+    console.log(tempIncorrectAnswer);
+    console.log(incorrectAnswersHoldingArray);
+    gameQuestions.push(temp);
+    gameAnswers.push(tempAnswer);
+    gameIncorrectAnswers.push(tempIncorrectAnswer);
+    questionsHoldingArray.splice(i, 1);
+    answersHoldingArray.splice(i, 1);
+    incorrectAnswersHoldingArray.splice(i, 1);
+    console.log(gameQuestions);
+    console.log(gameAnswers);
+    console.log(gameIncorrectAnswers);
+    x++;
+}
 
 function runQuiz() {
     console.log('play');
@@ -112,10 +113,13 @@ function inputQuestions() {
     getQuestionElement.innerText = gameQuestions[0]
     getAnswerElement.innerText = gameAnswers[0]
     getIncorrectAnswerElement.innerText = gameIncorrectAnswers[0]
+    
 }
 
 function checkAnswer() {
-
+    if (answers === true) {
+        scoreIncreaseElement = x++;
+    }
 }
 
 function updateScore() {
@@ -123,5 +127,5 @@ function updateScore() {
 }
 
 function nextQuestion() {
-    
+
 }
